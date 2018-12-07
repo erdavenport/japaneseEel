d00dee7c9f4a37006fc0341b3ae88f041c51d917
Emily Davenport <erdavenport@uwalumni.com>
2018-12-07 14:20:29 +0100

    script to count reads not mappe to nuclear genome

---
+++ scripts/count_reads_not_mapped_to_nuclear_genome_fastq_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will count the number of reads for each of the files containing reads not mapped to the nuclear genome:
+
+cd /data2/japaneseEel/data/STACKS_processed/2_unmapped_reads_from_nuclear_genome_bowtie/
+for i in *.fastq
+do
+echo -en "$i \t" >> ../../../results/1_general_info/reads_per_sample_not_mapped_to_nuclear_genome_fastq.txt
+LINES="$(cat $i | wc -l)"
+echo $((LINES/4)) >> ../../../results/1_general_info/reads_per_sample_not_mapped_to_nuclear_genome_fastq.txt
+done
+
+
+
+echo "DONE!"
+
+
