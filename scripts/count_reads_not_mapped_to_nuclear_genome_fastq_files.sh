c8ec7e903e2727bbab242b07e469039f5e97203f
Emily Davenport <erdavenport@uwalumni.com>
2018-12-07 14:24:28 +0100

    fix directory in count reads script

--- scripts/count_reads_not_mapped_to_nuclear_genome_fastq_files.sh
+++ scripts/count_reads_not_mapped_to_nuclear_genome_fastq_files.sh
@@ -2,7 +2,7 @@
 
 # This script will count the number of reads for each of the files containing reads not mapped to the nuclear genome:
 
-cd /data2/japaneseEel/data/STACKS_processed/2_unmapped_reads_from_nuclear_genome_bowtie/
+cd data/STACKS_processed/2_unmapped_reads_from_nuclear_genome_bowtie/
 for i in *.fastq
 do
 echo -en "$i \t" >> results/1_general_info/reads_per_sample_not_mapped_to_nuclear_genome_fastq.txt
