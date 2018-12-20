8c0d1748a6bd6d49c53265051b7ab5f621f32325
Emily Davenport <erdavenport@uwalumni.com>
2018-12-20 14:37:02 +0100

    update plotPCA legend location

--- scripts/plot_PCA.R
+++ scripts/plot_PCA.R
@@ -98,7 +98,7 @@ print("saving PCA plot")
 # Color:
 pdf(paste(outpath, "PCA.no.outgroup.",today,"ERD.pdf", sep=""), width=6, height=6)
 plot(evecsNoOut[,3], evecsNoOut[,4], pch=16, col = sapply(evecsNoOut$V1, pop.cols), cex=1.5, xlab=paste("PC 1 -", (PVENoOut[1]*100),"% of variance", sep=" "),ylab=paste("PC 2 -",(PVENoOut[2]*100),"% of variance",sep=" ")) 
-legend("bottomleft", cex=0.75, pch=16, col = sapply(forLegend, pop.cols), legend=forLegend, bty="n")
+legend("topright", cex=0.75, pch=16, col = sapply(forLegend, pop.cols), legend=forLegend, bty="n")
 hi <- dev.off()
 
   
